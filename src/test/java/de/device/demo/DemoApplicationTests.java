6f42f6af6359439b64c6baa9ed7776c5b6a515c6
Semyon Eganov <icolizer.semyon@gmail.com>
2025-12-17 14:47:42 +0100

    Initial spring boot generated app

---
+++ src/test/java/de/device/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.device.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
